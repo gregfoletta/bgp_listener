817bd6ae9347e61ee821f43908516553348748ba
Greg Foletta <Greg Foletta>
2015-11-13 10:19:51 +1100

    Added 'show withdrawn' command

--- main.c
+++ main.c
@@ -2,12 +2,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <pthread.h>
 
 #include "bgp.h"
+#include "cli.h"
 
+
+int test_func(void *);
 
 int main(int argc, char **argv) {
     struct bgp_peer *peer_1;
+    struct cli_command_list *list = NULL;
+
+    
+    cli_commandlist_add(&list, "show peer", print_bgp_peer_info);
+    cli_commandlist_add(&list, "show withdrawn", print_bgp_pending_withdrawn);
 
     struct bgp_local local_info = { 65000, 180, 0x01010101};
 
@@ -21,9 +30,15 @@ int main(int argc, char **argv) {
     bgp_connect(peer_1);
     bgp_open(peer_1, local_info);
 
-    bgp_loop(peer_1);
+    if (pthread_create(&(peer_1->thread), NULL, bgp_loop, peer_1) != 0) {
+        bgp_print_err("pthread_create() failed");
+        return 0;
+    }
+
+    cli_read_loop(list, peer_1);
 
     bgp_destroy_peer(peer_1);
 
     return 0;
 }
+
