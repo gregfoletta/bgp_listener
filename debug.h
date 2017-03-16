d89dac956aa08a0fe67104e2bf4e76e1ec2c97e9
grefoletta <greg@foletta.org>
2017-03-17 08:36:35 +1100

    Directory organisation

--- debug.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-void debug_enable();
-void debug_disable();
-int is_debug();
-
-#define DEBUG_PRINT(...) if (is_debug()) { fprintf(stderr, "[DEBUG] "); fprintf((stderr), __VA_ARGS__); }
